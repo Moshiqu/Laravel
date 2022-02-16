77c119efb5ac6fda74a7fb200dbf62ffd8098930
Moshiqu <912323520@qq.com>
2022-02-16 21:27:25 +0800

    样式修改

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,30 +6,33 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Bookstore App using Laravel 6.x</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('css/custom/custom.css') }}" rel="stylesheet" type="text/css" />
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
 </head>
 <body>
   <header> 
-    <nav> 
-      <center>
-        <table width="700"> 
-          <tr valign="bottom"> 
-            <td class="value"><a href="/"><img src="/images/logo.jpg" border="0"  width="180"/> </a></td> 
-            <td class="value"><a href="/"><img src="/images/home.png" border="0"  width="55"></a></td> 
-            <td class="value"><a href="/register"><img src="/images/register.png" border="0"  width="55"></a></td> 
-            <td class="value"><a href="/books"><img src="/images/cart.png" border="0"  width="55"></a></td> 
-            <td class="value"><a href="/books"><img src="/images/login.png" border="0"  width="55"></a></td> 
-            <td class="value"><a href="/"><img src="/images/admin.png" border="0"  width="55"></a></td> 
-          </tr> 
-        </table>  
-      </center>
+    <nav class='header_nav'> 
+        <table class='header_nav_table'> 
+          <tbody class='header_nav_table_body'>
+            <tr class='table_left'>
+              <td class="value" ><a href="/books"><img src="/images/logo.jpg" class='logo_box'/></a></td> 
+            </tr>
+            <tr class='table_content container' valign="bottom">
+              <td class="value"><a href="/books"><img src="/images/home.png">Home</a></td> 
+              <td class="value"><a href="/register"><img src="/images/register.png">Register</a></td> 
+              <td class="value"><a href="/books"><img src="/images/cart.png">Cart</a></td> 
+              <td class="value"><a href="/books"><img src="/images/login.png">Login</a></td> 
+              <td class="value"><a href="/"><img src="/images/admin.png">Admin</a></td> 
+            </tr>
+          </tbody>
+        </table>
     </nav> 
   </header>
   
-  <div class="container">
+  <section class="container">
     @yield('content')
-  </div>
-  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+  </section>
 
-<footer><center>Copyright 2021 Online Book Store</center></footer>
+  <footer class='center footer'>Copyright 2021 Online Book Store</footer>
 </body>
 </html>
